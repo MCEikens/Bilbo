f833861efc5d7a3e8d06afc90862ff9fc08ca19a
Dominik <DominikOtto.dominikotto1@web.de>
2017-10-18 11:22:16 +0200

    [FEATURE] add sql dump

--- theme/js/accordion.js
+++ theme/js/accordion.js
@@ -1,38 +1,16 @@
-var acc = [];
-acc = document.getElementsByClassName('accordion');
-console.log(acc);
-for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
+var acc = document.getElementsByClassName('accordion');
 
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
+function accordionToggle() {
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].onclick = function () {
+            this.classList.toggle("active");
+
+            var accordionSubs = this.nextElementSibling;
+            if (accordionSubs.style.display === "block") {
+                accordionSubs.style.display = "none";
+            } else {
+                accordionSubs.style.display = "block";
+            }
         }
     }
 }
-
-
-
-// function accordion() {
-//     var acc = [];
-//     acc = document.getElementsByClassName('accordion');
-//     console.log(acc);
-//     acc[i].onclick = function(){
-//
-//         this.classList.toggle("active");
-//
-//
-//         if (panel.style.display === "block") {
-//             panel.style.display = "none";
-//         } else {
-//             panel.style.display = "block";
-//         }
-//     }
-// }
-
