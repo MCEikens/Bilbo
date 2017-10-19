fe5965e82f627444b06461d6cdaff73c2032d3ca
Dominik <DominikOtto.dominikotto1@web.de>
2017-10-19 12:22:56 +0200

    [FEATURE] Styleedit --> Navleiste [BUGFIX]

--- theme/js/accordion.js
+++ theme/js/accordion.js
@@ -1,16 +1,17 @@
 var acc = document.getElementsByClassName('accordion');
 
-function accordionToggle() {
-    for (var i = 0; i < acc.length; i++) {
-        acc[i].onclick = function () {
-            this.classList.toggle("active");
+window.onload = function accordionToggle() {
 
-            var accordionSubs = this.nextElementSibling;
-            if (accordionSubs.style.display === "block") {
-                accordionSubs.style.display = "none";
-            } else {
-                accordionSubs.style.display = "block";
+        for (var i = 0; i < acc.length; i++) {
+            acc[i].onclick = function () {
+                this.classList.toggle("active");
+
+                var accordionSubs = this.nextElementSibling;
+                if (accordionSubs.style.display === "block") {
+                    accordionSubs.style.display = "none";
+                } else {
+                    accordionSubs.style.display = "block";
+                }
             }
         }
     }
-}
